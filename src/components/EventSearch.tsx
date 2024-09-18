bf1060835bbada067e97e397d3ddd32ce1289e63
Denis Krylov <dkrylovsb@users.noreply.github.com>
2024-09-18 16:39:20 -0400

    Added a "Clear" button

--- src/components/EventSearch.tsx
+++ src/components/EventSearch.tsx
@@ -85,6 +85,12 @@ export default function EventSearch({searchHeader, errorText, placeholder, searc
     }
   };
 
+  const resetForm = () => {
+    setDrugs(null)
+    setSearchQuery('')
+    setSearch('')
+  };
+
   const onFirstDataRendered = useCallback((params) => {
     params.api.sizeColumnsToFit();
   }, []);
@@ -125,6 +131,19 @@ export default function EventSearch({searchHeader, errorText, placeholder, searc
               viewBox="0 0 24 24"
               className="usa-icon usa-icon--size-3 usa-search__submit-icon" focusable="false" role="img"
               name="Search"
+            >
+              <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+            </svg>
+          </button>
+          <button className='minw-205 usa-button margin-top-2' style={{backgroundColor:"grey"}} type='button' onClick={resetForm}>
+            <span className="usa-search__submit-text">Clear</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              viewBox="0 0 24 24"
+              className="usa-icon usa-icon--size-3 usa-search__submit-icon" focusable="false" role="img"
+              name="Clear"
             >
               <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
             </svg>
